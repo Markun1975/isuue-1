2eabc7cefb50a7765db25f0288f39a590bcc838a
Masaki Sugita <choco_9031@yahoo.co.jp>
2020-11-02 13:59:32 +0900

    Initial Commit

---
+++ issue#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  issue#1
+//
+//  Created by Masaki on R 2/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
